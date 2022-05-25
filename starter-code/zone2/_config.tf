9ad0e4db63fe4a0ec840391a509668d3f059ac5c
Raditya Perdhevi <raditya.perdhevi@gmail.com>
2022-05-25 13:50:03 +0700

    updated config and ec2

--- starter-code/zone2/_config.tf
+++ starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-travis-west"
+     bucket = "udacity-tf-rperdhevi-west"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
